e9379bb29068a82ace70dbb97a54acd01005658a
Graham Watson <graham.watson@cakedc.com>
2016-11-26 01:34:08 -0500

    Allow null shortname (for <area nohref>)

--- src/Map/Target.php
+++ src/Map/Target.php
@@ -12,7 +12,7 @@ class Target
      * Construct method
      *
      * @param string $coords The coordinates in the imagemap
-     * @param string $shortName The short name, matching a method name in RoomsController
+     * @param string|null $shortName The short name, matching a method name in RoomsController
      * @param string $longName A longer name, used in HTML 'title' attributes
      * @param null|string $action Optional, if a non-default action is being conducted in a room
      * @return void
