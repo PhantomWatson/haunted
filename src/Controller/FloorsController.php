a086bc3e746308b3e1c04cda5c6b2d5a11ed49a2
Graham Watson <graham.watson@cakedc.com>
2016-11-26 20:24:49 -0500

    Add second floor

--- src/Controller/FloorsController.php
+++ src/Controller/FloorsController.php
@@ -3,9 +3,13 @@ namespace App\Controller;
 
 class FloorsController extends AppController
 {
-
     public function first()
     {
-        $this->set();
+
+    }
+
+    public function second()
+    {
+
     }
 }
